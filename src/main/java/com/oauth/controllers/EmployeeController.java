8bce5753f16514c8c4066f25c64525608f066c74
pankajbhamoriya <38315265+pankajbhamoriya@users.noreply.github.com>
2020-01-12 13:19:50 +0530

    coommit

--- src/main/java/com/oauth/controllers/EmployeeController.java
+++ src/main/java/com/oauth/controllers/EmployeeController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,7 +22,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oauth.model.Employee;
 
-@Controller
+//@Controller
+@RestController
 public class EmployeeController {
 
 	@RequestMapping(value = "/getEmployees", method = RequestMethod.GET)
