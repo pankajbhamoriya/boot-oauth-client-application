d66a12318cc8638c43ddc0bfd66601402e7dc5ed
pankajbhamoriya <38315265+pankajbhamoriya@users.noreply.github.com>
2020-01-12 11:32:13 +0530

    commit

--- src/main/java/com/oauth/SpringBootFormHandingApplication.java
+++ src/main/java/com/oauth/SpringBootFormHandingApplication.java
@@ -2,11 +2,18 @@ package com.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class SpringBootFormHandingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFormHandingApplication.class, args);
+	}
+	
+	@GetMapping("/get")
+	public ModelAndView getEmployeeInfo() {
+		return new ModelAndView("getEmployees");
 	}
 }
